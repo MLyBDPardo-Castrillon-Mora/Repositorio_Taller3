dbfc049776a02ebe6de9e89cdd1bf86cabdd7c5f
FernandoCastrillonGarcia <f.castrillon@uniandes.edu.co>
2023-02-21 21:26:06 -0500

    Modelo fallido con train_hog

--- Script/Taller_2.R
+++ Script/Taller_2.R
@@ -20,17 +20,35 @@ p_load("tidyverse", "stargazer", "caret", "leaps", "Hmisc")
 #train_per <- read.csv("./Stores/train_personas.csv")
 train_hog <- read.csv("./Stores/train_hogares.csv")
 #test_per <- read.csv("./Stores/test_personas.csv")
-#test_hog <- read.csv("./Stores/test_hogares.csv")
-
+test_hog <- read.csv("./Stores/test_hogares.csv")
+colnames(train_hog)
 #MODIFICAR DATOS
 #=================================================================================
-  #Etiquetar datos
-  y_train_h <- train_hog %>% select(Pobre) #Variable Respuesta
-  x_train_h <- train_hog %>% select(-Ingtotug,-Ingpcug,-Dominio,
-                                    -Fex_dpto,-Fex_c, -id,-Pobre)
-  colnames(x_train_h)[2:7] = c("NCuartos","NCuartosDormir","ViviendaEs","CuotaAmort",
+  #Etiquetar datos de entrenamiento
+
+  colnames(train_hog)[4:9] = c("NCuartos","NCuartosDormir","ViviendaEs","CuotaAmort",
                                "ArriendoHipo","Arriendo")
-  colnames(x_train_h)
   
+  train_hog<-train_hog %>% mutate(Pobre=factor(Pobre,levels=c(1,0),labels=c("Sí","No"))) 
   
+  y_train <- train_hog$Pobre
+  y_train <- as.factor(y_train)
+  x_train <- train_hog %>% select(-Pobre,-Fex_dpto,-Fex_c,-id)
+  x_train <- as.matrix(x_train)
+#MODELO
+#==========================================================================================
+fiveStats <- function(...) c(twoClassSummary(...), defaultSummary(...))
   
+ctrl<- trainControl(method = "cv",
+                    number = 10,
+                    summaryFunction = fiveStats, 
+                    classProbs = TRUE,
+                    verbose=FALSE,
+                    savePredictions = "final")
+
+modelo <- train(y = y_train,
+                x = x_train,
+                method = "glmStepAIC",
+                trControl = ctrl,
+                maxvar = 10
+                )
