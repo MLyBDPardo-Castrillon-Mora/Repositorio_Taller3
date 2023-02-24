f680b173630db6105ffcdc62abeb73d01314fb78
RiemannCauchy <123638911+RiemannCauchy@users.noreply.github.com>
2023-02-24 12:15:53 -0500

    Con las var

--- Script/Base Taller 2 - JFM.R
+++ Script/Base Taller 2 - JFM.R
@@ -1,5 +1,6 @@
 
 # LIBRERIAS
+library(dplyr)
 library(e1071)
 library(MASS)
 
@@ -11,9 +12,53 @@ test_hogares <- read.csv("~/Downloads/uniandes-bdml-20231-ps2/test_hogares.csv")
 train_personas <- read.csv("~/Downloads/uniandes-bdml-20231-ps2/train_personas.csv")
 test_personas <- read.csv("~/Downloads/uniandes-bdml-20231-ps2/test_personas.csv")
 
+# Var Discretas
+train_personas$Clase
+train_personas$sexo
+train_personas$P6020
+train_personas$Estrato1
+train_personas$P6040
+train_personas$P6050
+train_personas$P6090
+train_personas$P6210
+train_personas$P6100
+train_personas$P6585s1
+train_personas$P6585s3
+train_personas$P6590
+train_personas$P6585s4
+train_personas$P6610
+train_personas$P6620
+train_personas$P6870
+train_personas$P6920
+train_personas$P7040
+train_personas$P7090
+train_personas$P7050
+train_personas$P7495
+train_personas$Pet
+train_personas$Des
+train_personas$Oc
+train_personas$Ina
+
+# Var Continaus
+train_personas$P5130
+train_personas$Nper
+train_personas$P6040
+train_personas$P6210s1
+train_personas$P6500
+train_personas$P6510s1
+train_personas$P6580s1
+train_personas$P6800
+train_personas$P7422s1
+train_personas$P7500s1a1
+train_personas$Ingtotob
+train_personas$Ingtot
+
 # Union de las bases de datos
 train_merge <- merge(train_personas,train_hogares,by='id')
 test_merge <- merge(test_personas,test_hogares,by='id')
+
+
+
 
 # LOGIT
 
@@ -65,5 +110,10 @@ nb.fit <- naiveBayes(
 nb.class <- predict(nb.fit, train_merge)
 table(nb.class, train_merge$Pobre)
 (294005+81355)/(294005+81355+55166+112583)
+
+
+
+
+
 
 
