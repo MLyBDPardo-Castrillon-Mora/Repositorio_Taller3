4cf5d0b04d01e46473a5291399d5f78dfcd76846
RiemannCauchy <123638911+RiemannCauchy@users.noreply.github.com>
2023-02-25 11:26:06 -0500

    NA problem - check :)

--- Script/Base Taller 2 - JFM.R
+++ Script/Base Taller 2 - JFM.R
@@ -1,16 +1,25 @@
 
-# LIBRERIAS
+# LIBRERIAS ====================================================================
 library(dplyr)
 library(e1071)
 library(MASS)
 
-# DATOS
+# DATOS ========================================================================
 
-# Importacion
+# Base de datos ----------------------------------------------------------------
+
+# Importacion de datos
 train_hogares <- read.csv("~/Downloads/uniandes-bdml-20231-ps2/train_hogares.csv")
 test_hogares <- read.csv("~/Downloads/uniandes-bdml-20231-ps2/test_hogares.csv")
 train_personas <- read.csv("~/Downloads/uniandes-bdml-20231-ps2/train_personas.csv")
 test_personas <- read.csv("~/Downloads/uniandes-bdml-20231-ps2/test_personas.csv")
+
+# Union de las bases de datos
+train_merge <- merge(train_personas,train_hogares,by='id')
+test_merge <- merge(test_personas,test_hogares,by='id')
+
+# Seleccion de datos -----------------------------------------------------------
+
 
 # Var Discretas
 train_personas$Clase
@@ -53,33 +62,69 @@ train_personas$P7500s1a1
 train_personas$Ingtotob
 train_personas$Ingtot
 
-# Union de las bases de datos
-train_merge <- merge(train_personas,train_hogares,by='id')
-test_merge <- merge(test_personas,test_hogares,by='id')
+# Base de datos - aux_RC
+train_RC <- train_merge %>% dplyr::select(
+  Estrato1, Pobre, P5000, P5010, Nper, P6020, P6040, P6090, P6210s1,P6585s1, 
+  P6585s3, P6590, P6620, P6800, P6920, P7040, P7510s3, P6050)
 
+test_RC <- test_merge %>% dplyr::select(
+  P5000, P5010, Nper, P6020, P6040, P6090, P6210s1,P6585s1, 
+  P6585s3, P6590, P6620, P6800, P6920, P7040, P7510s3, P6050)
 
+# Limpieza de datos ------------------------------------------------------------
 
+# Contar NAs en la base de datos
+colSums(is.na(train_RC)) %>% as.data.frame()
 
-# LOGIT
+# Eliminar columnas con DEMASIADOS NAs
+train_RC <- train_RC %>% dplyr::select(-P6590, -P6620, -P6800, -P7040, -P7510s3)
+
+# Arreglamos P6585s1 y P6585s3 con su media si el estrato es 'bajo'...
+train_RC$P6585s1[is.na(train_RC$P6585s1) == "TRUE" & train_RC$Estrato1>1] <- 0
+train_RC$P6585s3[is.na(train_RC$P6585s3) == "TRUE" & train_RC$Estrato1>1] <- 0
+
+length(which(train_RC$P6585s1 == "0"))
+length(which(train_RC$P6585s3 == "0"))
+
+# Crear lista auxiliar de NAs
+list_na_train <- colnames(train_RC)[apply(train_RC, 2, anyNA)]
+
+# Media por Variable
+average_missing_train <- apply(train_RC[,colnames(train_RC) %in% list_na_train],
+                         2,
+                         mean,
+                         na.rm =  TRUE)
+average_missing_train
+
+# Reemplazamos medias en los NAs - Creamos nueva base de datos
+train_prueba <- train_RC %>%
+  mutate(P6090_n  = ifelse(is.na(P6090), average_missing_train[1], P6090),
+         P6210s1_n  = ifelse(is.na(P6210s1), average_missing_train[2], P6210s1),
+         P6585s1_n  = ifelse(is.na(P6585s1), average_missing_train[3], P6585s1),
+         P6585s3_n  = ifelse(is.na(P6585s3), average_missing_train[4], P6585s3),
+         P6920_n = ifelse(is.na(P6920), average_missing_train[5], P6920))
+
+# Checkear NAs en variables con medias
+colSums(is.na(train_prueba)) %>% as.data.frame()
+
+#MODELOS =======================================================================
+
+# LOGIT ------------------------------------------------------------------------
 
 # Modelo
-glm.fits <- glm(Pobre ~ P5000 + Nper + P6210s1 + Ingtot + Des + P6050,
-                data = train_merge, family = "binomial")
+glm.fits <- glm(Pobre ~ P5000 + P5010 + Nper + P6020 + P6040 + P6090_n + P6210s1_n + P6585s1_n + P6585s3_n + P6920_n + P6050,
+                data = train_prueba, family = "binomial")
 summary(glm.fits)
 
 # Train check
 glm.probs <- predict(glm.fits, type = "response")
 glm.pred <- rep("No-pobre", length(fitted(glm.fits)))
-glm.pred[glm.probs > .6] = "Pobre"
+glm.pred[glm.probs > 0.4] = "Pobre"
 table(glm.pred, glm.fits$y)
 
-# Test!
-glm.probs <- predict(glm.fits, test_merge , type = "response")
-glm.pred <- rep("No-pobre", length(fitted(glm.fits)))
-glm.pred[glm.probs > .6] = "Pobre"
-table(glm.pred, glm.fits$y)
+# LDA --------------------------------------------------------------------------
 
-# LDA
+#Modelo de prueba
 
 lda.fit <- lda(
   Pobre ~ P5000 + Nper + P6210s1 + Ingtot + P6050,
@@ -88,13 +133,32 @@ lda.fit
 plot(lda.fit)
 lda.pred <- predict(lda.fit, train_merge)
 lda.class <- lda.pred$class
-table(lda.class, train_merge$Pobre)
+table(lda.class, test_merge$Pobre)
 (338454+16400)/(338454+16400+83371+9434)
+
+# Modelo 1
+lda.fit <- lda(
+  Pobre ~ P5000 + P5010 + Nper + P6020 + P6040 + P6090_n + P6210s1_n + P6585s1_n + P6585s3_n + P6920_n + P6050,
+  data = train_prueba)
+lda.fit
+lda.pred <- predict(lda.fit, train_prueba)
+lda.class <- lda.pred$class
+table(lda.class, train_prueba$Pobre)
+(23803+602)/(23803+602+1106+780)
+
+# Modelo 2
+lda.fit <- lda(
+  Pobre ~ P5000 + Nper + P6210s1_n + P6050, data = train_prueba)
+lda.fit
+lda.pred <- predict(lda.fit, train_prueba)
+lda.class <- lda.pred$class
+table(lda.class, train_prueba$Pobre)
+(23803+602)/(23803+602+1106+780)
 
 # QDA
 
 qda.fit <- qda(
-  Pobre ~ P5000 + Nper + P6210s1 + Ingtot + P6050,
+  Pobre ~ P5000 + Nper + P6210s1_n + P6050,
   data = train_merge)
 qda.fit
 qda.pred <- predict(qda.fit, train_merge)
