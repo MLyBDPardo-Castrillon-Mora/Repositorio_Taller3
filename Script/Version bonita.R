053cb257fed353cc23f4b1c28c04b1bc97e9ea75
RiemannCauchy <123638911+RiemannCauchy@users.noreply.github.com>
2023-02-26 21:17:49 -0500

    final

--- Script/Version bonita.R
+++ Script/Version bonita.R
@@ -8,6 +8,7 @@ library(ISLR2)
 library(leaps)
 library(MASS)
 library(pacman)
+library(randomForest)
 library(rpart)
 library(smotefamily)
 library(tidyverse)
@@ -18,6 +19,9 @@ library(tidyverse)
 
 # Semilla
 set.seed(4040)
+
+# Limite de memoria
+memory.limit(100000)
 
 # Importacion de datos
 db.h <- read.csv("~/Downloads/uniandes-bdml-20231-ps2/train_hogares.csv")
@@ -77,22 +81,24 @@ colnames(dev_set) <- c("id", "rooms", "bedrooms", "nper", "ayuda_1", "ayuda_2",
 # Crear variable "rent_est"
 train$ayuda_1 <- ifelse(is.na(train$ayuda_1), 0, train$ayuda_1)
 train$ayuda_2 <- ifelse(is.na(train$ayuda_2), 0, train$ayuda_2)
-train$rent_rec <- train$ayuda_1 + train$ayuda_2
+train$rent_est <- train$ayuda_1 + train$ayuda_2
 train <- dplyr::select(train, -ayuda_1, -ayuda_2)
 
 # Repetir para test
 test$ayuda_1 <- ifelse(is.na(test$ayuda_1), 0, test$ayuda_1)
 test$ayuda_2 <- ifelse(is.na(test$ayuda_2), 0, test$ayuda_2)
-test$rent_rec <- test$ayuda_1 + test$ayuda_2
+test$rent_est <- test$ayuda_1 + test$ayuda_2
 test <- dplyr::select(test, -ayuda_1, -ayuda_2)
 
 # Repetir para dev_set
 dev_set$ayuda_1 <- ifelse(is.na(dev_set$ayuda_1), 0, dev_set$ayuda_1)
 dev_set$ayuda_2 <- ifelse(is.na(dev_set$ayuda_2), 0, dev_set$ayuda_2)
-dev_set$rent_rec <- dev_set$ayuda_1 + dev_set$ayuda_2
+dev_set$rent_est <- dev_set$ayuda_1 + dev_set$ayuda_2
 dev_set <- dplyr::select(dev_set, -ayuda_1, -ayuda_2)
 
 # Resolver NAs -----------------------------------------------------------------
+
+# TRAIN
 
 # Contar NAs
 colSums(is.na(train)) %>% as.data.frame()
@@ -134,6 +140,47 @@ train$health_aff <- aux$health_aff
 # Check - Conteo de NAs
 colSums(is.na(train)) %>% as.data.frame()
 
+# TEST
+
+# Contar NAs
+colSums(is.na(test)) %>% as.data.frame()
+
+# Imputacion por Arboles de Decision
+
+# I. Health
+aux <- subset(test, select = c("health", "estrato", "age", "nper", "ing"))
+aux$health <- ifelse(aux$health == "9", NA, aux$health)
+tree_model <- rpart(health ~ ., data = aux, method = "class")
+aux$health[is.na(aux$health)] <- predict(tree_model, newdata = aux[is.na(aux$health), ], type = "class")
+aux$health <- ifelse(aux$health == "1", 0, 1)
+test$health <- aux$health
+
+# II. Help
+aux <- subset(test, select = c("help", "estrato", "age", "nper", "ing"))
+tree_model <- rpart(help ~ ., data = aux)
+aux$help[is.na(aux$help)] <- predict(tree_model, newdata = aux[is.na(aux$help), ])
+test$help <- aux$help
+
+# III. Educ
+aux <- subset(test, select = c("educ", "estrato", "age", "nper", "ing"))
+tree_model <- rpart(educ ~ ., data = aux, method = "class")
+aux$educ[is.na(aux$educ)] <- predict(tree_model, newdata = aux[is.na(aux$educ), ], type = "class")
+test$educ <- aux$educ
+
+# IV. Grade
+aux <- subset(test, select = c("grade", "estrato", "age", "nper", "ing"))
+tree_model <- rpart(grade ~ ., data = aux)
+aux$grade[is.na(aux$grade)] <- predict(tree_model, newdata = aux[is.na(aux$grade), ])
+test$grade <- aux$grade
+
+# V. Health_aff
+aux <- subset(test, select = c("health_aff", "estrato", "age", "nper", "ing"))
+tree_model <- rpart(health_aff ~ ., data = aux, method = "class")
+aux$health_aff[is.na(aux$health_aff)] <- predict(tree_model, newdata = aux[is.na(aux$health_aff), ], type = "class")
+test$health_aff <- aux$health_aff
+
+# Check - Conteo de NAs
+colSums(is.na(test)) %>% as.data.frame()
 
 # DUMMIES  =================================================================
 
@@ -147,6 +194,13 @@ dev_set[, cols_f] <- data.frame(apply(dev_set[cols_f], 2, as.factor))
 train <- dummy_cols(train, select_columns = cols_f)
 test <- dummy_cols(test, select_columns = cols_f)
 dev_set <- dummy_cols(dev_set, select_columns = cols_f)
+
+# AGRUPAR HOGARES ==============================================================
+
+# Clasificador por hogares
+train_h <- train[train$parent=="1",]
+test_h <- test[test$parent=="1",]
+dev_set_h <- dev_set[dev_set$parent=="1",]
 
 
 # BALANCEO DE DATOS ============================================================
@@ -180,6 +234,15 @@ names(smote)[names(smote) == "class"] <- "poor"
 hist(as.numeric(smote$poor), col="coral")
 prop.table(table(smote$poor))
 
+#3. Undersampling para hogares
+
+aux <- train_h[, 2:46]
+under_h <- downSample(x = aux[, -ncol(aux)], y = as.factor(aux$poor))
+under_h <- dplyr::select(under_h, -"Class")
+
+# Revisar correcion de balance
+hist(as.numeric(undersample_h$poor), col="coral")
+prop.table(table(undersample_h$poor))
 
 # MODELOS INGENUOS =============================================================
 
@@ -459,78 +522,123 @@ m5_acc <- table(m5_pred, test$poor)
 # Accuracy - 74.14%
 confusionMatrix(m5_acc)
 
-# AGRUPAR HOGARES ==============================================================
 
-# Clasificador por hogares
-train_h <- train[train$parent=="1",]
-test_h <- test[test$parent=="1",]
-dev_set_h <- dev_set[dev_set$parent=="1",]
+# Modelo 6: Random Forest ------------------------------------------------------]
 
-model.4 <- lda(as.numeric(poor) ~ rooms + bedrooms + nper + npersug + clase
-               + sex + age + rent_rec + help + grade + prop_2 + prop_3
-               + prop_4 + prop_5 + prop_6 + health_aff_2 + health_aff_3
-               + health_aff_9 + educ_1 + educ_2 + educ_3 + educ_4 + educ_5, 
-               data = train_h)
-model.4
-model.4.pred <- predict(model.4, test_h)
-model.4.class <- model.4.pred$class
-m4_acc <- table(model.4.class, test_h$poor)
+# Estimacion 6.0: Ingenuo
+under_h <- dplyr::select(under_h, -"poor", -"rent_rec", -"estrato", -"help")
+under_h <- dplyr::select(under_h, -"parent_2", -"parent_3", -"parent_4", -"parent_5", -"parent_6", -"parent_7",-"parent_8",-"parent_9")
+model.6 <- randomForest(ing ~ ., data = train_h, ntree = 2)
+m6_probs <- predict(model.6, newdata = test_h)
 
-# Accuracy - 82.98%
-confusionMatrix(m4_acc)
+# Matriz de confusion
+m6_pred <- rep(0, length(m6_probs))
+m6_pred[m6_probs < test_h$lp] <- 1
+m6_acc <- table(m6_pred, test_h$poor)
 
+# Accuracy - 83.14%
+confusionMatrix(m6_acc)
 
-# SUBSET SELECTION =============================================================
+# Estimacion 6.1: Undersampling
+model.6 <- randomForest(ing ~ ., data = under_h, ntree = 4)
+m6_probs <- predict(model.6, newdata = test_h)
 
-sub_sel <- regsubsets(poor ~ .,
-                      data = dev_set_h,
-                      nvmax = 15,
-                      method = "forward")
+# Matriz de confusion
+m6_pred <- rep(0, length(m6_probs))
+m6_pred[m6_probs < test_h$lp] <- 1
+m6_acc <- table(m6_pred, test_h$poor)
 
-sum.sub_sel <- summary(sub_sel)
+# Accuracy - 83.14%
+confusionMatrix(m6_acc)
 
-# Metricas
-Adj.R2 <- which.max(sum.sub_sel$adjr2)
-CP <- which.min(sum.sub_sel$cp)
-BIC <- which.min(sum.sub_sel$bic)
-data.frame(Adj.R2, CP, BIC)
+# Estimacion 6.2: Undersampling - 16 trees
+model.6 <- randomForest(ing ~ ., data = under_h, ntree = 16)
+m6_probs <- predict(model.6, newdata = test_h)
 
-as.data.frame(coef(sub_sel, 12))
+# Matriz de confusion
+m6_pred <- rep(0, length(m6_probs))
+m6_pred[m6_probs < test_h$lp] <- 1
+m6_acc <- table(m6_pred, test_h$poor)
 
-# Comparacion
-lm.0 <- glm(as.numeric(class)~., data = tr_b, family="binomial")
-lm.probs.0 <- predict(lm.0, type="response")
-lm.pred.0 <- rep(0, length(lm.probs.0))
-lm.pred.0[lm.probs.0 > 0.5] <- 1
-table(lm.pred.0, tr_b$class)
+# Accuracy - 83.14%
+confusionMatrix(m6_acc)
 
-confusionMatrix(data = as.factor(lm.pred.0), 
-                reference = as.factor(tr_b$class), 
-                positive="1")
+# Estimacion 6.2: Undersampling - 64 trees
+model.6 <- randomForest(ing ~ ., data = under_h, ntree = 64)
+m6_probs <- predict(model.6, newdata = test_h)
 
-lm.1 <- glm(as.numeric(class) ~ clase + age + parent + rooms + bedrooms 
-            + property + nper + npersug + ingtotug + npobres + sex_2, data=tr_b, 
-            family = "binomial")
-lm.probs.1 <- predict(lm.1, type="response")
-lm.pred.1<- rep(0, length(lm.probs.1))
-lm.pred.1[lm.probs.1 > 0.5] <- 1
-table(lm.pred.1, tr_b$class)
+# Matriz de confusion
+m6_pred <- rep(0, length(m6_probs))
+m6_pred[m6_probs < test_h$lp] <- 1
+m6_acc <- table(m6_pred, test_h$poor)
 
-# Modelo de Ignacio
-levels(tr_b$class) <- c("no_pobre", "pobre")
-model.1.smote <- train(as.factor(class) ~ as.factor(clase_2) + as.factor(sex_2) + rooms + bedrooms + as.factor(property_2) + as.factor(property_3) + as.factor(property_4) + as.factor(property_5) + as.factor(property_6) + nper,
-                            data = tr_b, 
-                            method = "glmnet",
-                            trControl = ctrl,
-                            family = "binomial", 
-                            metric = "ROC",
-)
-
-mylogit_lasso_smote
+# Accuracy - 83.14%
+confusionMatrix(m6_acc)
 
 
+# EXPORT =======================================================================
 
+# Resolver NAs de Kaggle
 
+kaggle <- dplyr::select(kaggle, id, P5000, P5010, Nper, P5130, P5140, Npersug,  
+                      Lp, Clase.x, P5090, P6020, P6040, P6090, P7495,
+                      P6050, P6100, P6210, P6210s1)
 
+colnames(kaggle) <- c("id", "rooms", "bedrooms", "nper", "ayuda_1", "ayuda_2", 
+                       "npersug", "lp", "clase", "prop", "sex", "age", "health",
+                       "rent_rec", "parent", "health_aff", "educ", "grade")
 
+kaggle_h <- kaggle[kaggle$parent=="1",]
 
+# Crear variable "rent_est"
+kaggle_h$ayuda_1 <- ifelse(is.na(kaggle_h$ayuda_1), 0, kaggle_h$ayuda_1)
+kaggle_h$ayuda_2 <- ifelse(is.na(kaggle_h$ayuda_2), 0, kaggle_h$ayuda_2)
+kaggle_h$rent_est <- kaggle_h$ayuda_1 + kaggle_h$ayuda_2
+kaggle_h <- dplyr::select(kaggle_h, -ayuda_1, -ayuda_2)
+
+# Contar NAs
+colSums(is.na(kaggle_h)) %>% as.data.frame()
+
+# Imputacion por Arboles de Decision
+
+# I. Health
+aux <- subset(kaggle_h, select = c("health", "age", "nper"))
+aux$health <- ifelse(aux$health == "9", NA, aux$health)
+tree_model <- rpart(health ~ ., data = aux, method = "class")
+aux$health[is.na(aux$health)] <- predict(tree_model, newdata = aux[is.na(aux$health), ], type = "class")
+aux$health <- ifelse(aux$health == "1", 0, 1)
+kaggle_h$health <- aux$health
+
+# II. Grade
+aux <- subset(kaggle_h, select = c("grade", "age", "nper"))
+tree_model <- rpart(grade ~ ., data = aux)
+aux$grade[is.na(aux$grade)] <- predict(tree_model, newdata = aux[is.na(aux$grade), ])
+kaggle_h$grade <- aux$grade
+
+# III. Health_aff
+aux <- subset(kaggle_h, select = c("health_aff", "age", "nper"))
+tree_model <- rpart(health_aff ~ ., data = aux, method = "class")
+aux$health_aff[is.na(aux$health_aff)] <- predict(tree_model, newdata = aux[is.na(aux$health_aff), ], type = "class")
+kaggle_h$health_aff <- aux$health_aff
+
+# Check - Conteo de NAs
+colSums(is.na(kaggle_h)) %>% as.data.frame()
+
+# Dummies
+kaggle_h[, cols_f] <- data.frame(apply(kaggle_h[cols_f], 2, as.factor))
+
+# Crear Dummies
+kaggle_h <- dummy_cols(kaggle_h, select_columns = cols_f)
+
+# Estimacion 6.2: Undersampling - 16 trees
+model.6 <- randomForest(ing ~ ., data = under_h, ntree = 64)
+m6_probs <- predict(model.6, newdata = kaggle_h)
+
+# Matriz de confusion
+m6_pred <- rep(0, length(m6_probs))
+m6_pred[m6_probs < kaggle_h$lp] <- 1
+
+export <- cbind(kaggle_h$id, m6_pred)
+export <- as.data.frame(export)
+colnames(export) <- c("id", "pobre")
+write.csv(export, "C:\\Users\\Usuario\\Downloads", row.names=FALSE)
